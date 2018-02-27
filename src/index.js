f575aab1e8b329fb50e4a25ade47c4c68106b8f6
Anastasiya957 <anastasiyabarysheva95@gmail.com>
2018-02-28 00:24:24 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let numOfFive = 0;
+  let p = 5;
+	
+  while (p <= number) {
+  	numOfFive += Math.floor(number/p);
+	p *= 5;
+  }
+  return numOfFive;
 }
